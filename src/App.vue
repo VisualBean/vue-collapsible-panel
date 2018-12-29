2b6ed1d4fd5fd3bc64b256495f4a35fa4d76164f
Alex <alekcarlsen@gmail.com>
2018-12-29 23:02:00 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="columns is-vcentered has-text-centered">
+      <div class="column is-4">
+        <Panel title="First Panel" id="frontpage-firstpanel">Some Content</Panel>
+      </div>
+      <div class="column">
+        <Panel title="Second Panel" id="frontpage-secondpanel">Some more Content
+          <template slot="footer">
+            <div class="footer-buttons">
+              <button class="button is-primary is-pulled-left">Button</button>
+              <button class="button is-danger is-pulled-right">Oh no!</button>
+            </div>
+          </template>
+        </Panel>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Panel from "./components/CollapsiblePanel.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Panel
   }
-}
+};
 </script>
 
-<style>
+<style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
+.footer-buttons {
+  padding: 0.75rem;
+  width: 100%;
+}
+@import "~bulma";
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import {
+  library
+} from '@fortawesome/fontawesome-svg-core'
+import {
+  faChevronDown,
+  faChevronUp
+} from '@fortawesome/free-solid-svg-icons'
+import {
+  FontAwesomeIcon
+} from '@fortawesome/vue-fontawesome'
+
+library.add(faChevronDown, faChevronUp)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 Vue.config.productionTip = false
 
 new Vue({
